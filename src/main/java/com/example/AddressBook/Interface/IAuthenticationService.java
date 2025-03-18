564df513dda9d7a39d51d68a672ca7df38086ce8
AVIRALSINGH67 <aviral.raghuvansh1411@gmail.com>
2025-03-18 10:58:12 +0530

    Uer login, Forget and reset Password added

--- src/main/java/com/example/AddressBook/Interface/IAuthenticationService.java
+++ src/main/java/com/example/AddressBook/Interface/IAuthenticationService.java
@@ -2,8 +2,12 @@ package com.example.AddressBook.Interface;
 
 
 import com.example.AddressBook.dto.AuthUserDTO;
+import com.example.AddressBook.dto.LoginDTO;
 import com.example.AddressBook.model.AuthUser;
 public interface IAuthenticationService {
     AuthUser register(AuthUserDTO userDTO) throws Exception;
+    String login(LoginDTO loginDTO);
+    String forgotPassword(String email, String newPassword);
+    String resetPassword(String email, String currentPassword, String newPassword);
 
 }
---
+++ src/main/java/com/example/AddressBook/Util/JwtToken.java
@@ -0,0 +1,35 @@
+package com.example.AddressBook.Util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtToken {
+    private static final String TOKEN_SECRET = "Lock";
+
+    public String createToken(Long id) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
+            return JWT.create()
+                    .withClaim("user_id", id)
+                    .sign(algorithm);
+        } catch (IllegalArgumentException | JWTCreationException e) {
+            throw new RuntimeException("Error while creating JWT token: " + e.getMessage());
+        }
+    }
+
+    public Long decodeToken(String token) {
+        try {
+            JWTVerifier verifier = JWT.require(Algorithm.HMAC256(TOKEN_SECRET)).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getClaim("user_id").asLong();
+        } catch (JWTVerificationException e) {
+            throw new RuntimeException("Invalid or expired token.");
+        }
+    }
+}
